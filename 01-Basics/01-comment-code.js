8e648e5619ffa9b95d1171767c2a09b4f7d2dd17
ani03sha <anirudh03sharma@gmail.com>
2019-01-14 11:16:45 +0530

    Added challenge 01

---
+++ 01-Basics/01-comment-code.js
@@ -0,0 +1,24 @@
+/**
+ * Comments are lines of code that JavaScript will intentionally ignore. 
+ * Comments are a great way to leave notes to yourself and to other people 
+ * who will later need to figure out what that code does.
+ * 
+ * There are two ways to write comments in JavaScript:
+ * 
+ * 1. Using "//"" will tell JavaScript to ignore the remainder of the text on the current line:
+ * -->> // This is an in-line comment.
+ * 
+ * 2. You can make a multi-line comment beginning with "/*" and ending with "*".
+ * -->> /* This is a multi-line 
+ *          comment 
+ * */
+
+
+// Solution -
+
+// This is a single line comment
+
+/**
+ * This is a multi
+ * line comment
+ */
