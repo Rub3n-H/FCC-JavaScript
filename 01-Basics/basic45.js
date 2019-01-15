a3e0d153e5934ee9f98d790b2720cde1dee760c4
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-01-15 12:16:16 +0530

    Added challenge 45 to Basics

---
+++ 01-Basics/basic45.js
@@ -0,0 +1,12 @@
+/**
+ * Create a shopping list in the variable myList. The list should be a multi-dimensional array 
+ * containing several sub-arrays.
+ * The first element in each sub-array should contain a string with the name of the item. 
+ * The second element should be a number representing the quantity i.e.
+ * ["Chocolate Bar", 15]
+ * 
+ * There should be at least 5 sub-arrays in the list.
+ */
+
+var myList = [["Cricket Bats", 5], ["Cricket Balls", 20], ["Batting Gloves", 5], 
+["Batting Pads", 5], ["Helmets", 10]];
