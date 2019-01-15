954c0ea6dafd7859815ced899fc63fe0ce09344f
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-01-15 14:39:41 +0530

    Added challenge 51 to Basics

---
+++ 01-Basics/basic51.js
@@ -0,0 +1,26 @@
+/**
+ * We can pass values into a function with arguments. 
+ * You can use a return statement to send a value back out of a function.
+ * 
+ * Example
+ * function plusThree(num) {
+ *   return num + 3;
+ * }
+ * 
+ * var answer = plusThree(5); // 8
+ * plusThree takes an argument for num and returns a value equal to num + 3.
+ * 
+ * Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value. 
+ */
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+function timesFive(param) {
+   return param * 5;
+}
+
+console.log(minusSeven(10));
+console.log(timesFive(9));
