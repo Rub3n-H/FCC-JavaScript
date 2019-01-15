f7c79619ac59e804e05ce98f0122778257174070
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-01-15 11:46:47 +0530

    Added challege 37 to Basics

---
+++ 01-Basics/basic37.js
@@ -0,0 +1,12 @@
+/**
+ * You can also nest arrays within other arrays, like this: [["Bulls", 23], ["White Sox", 45]]. 
+ * This is also called a Multi-dimensional Array.
+ * 
+ * Create a nested array called myArray.
+ */
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Solution
+var myArray = [["Digital", 10], ["Analog", 0.123456789]];
