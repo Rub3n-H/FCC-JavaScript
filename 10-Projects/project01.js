491907a67853afabb3b656c708e3d3c56b86ef4e
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-01-31 21:21:19 +0530

    Added challenge 01 to Projects

---
+++ 10-Projects/project01.js
@@ -0,0 +1,31 @@
+/**
+ * Return true if the given string is a palindrome. Otherwise, return false.
+ * 
+ * A palindrome is a word or sentence that's spelled the same way both forward and backward, 
+ * ignoring punctuation, case, and spacing.
+ * 
+ * Note
+ * You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn 
+ * everything into the same case (lower or upper case) in order to check for palindromes.
+ * 
+ * We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+ * 
+ * We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+ */
+
+
+function palindrome(str) {
+
+    // Remove all the special characters in the string
+    str = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+
+    // Iterate the string to compare characters
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(palindrome("A man, a plan, a canal. Panama"));
